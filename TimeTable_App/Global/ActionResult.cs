b4902fa6c17ce02c4dae9c3dcbfa6d7c2eaa34db
KusalPriyanka <kusalpriyanka782@gmail.com>
2020-08-14 22:55:06 +0530

    Added ActionResult class

---
+++ TimeTable_App/Global/ActionResult.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+/*
+ *      Class Name      -   ActionResult
+ *      Author          -   Kusal Perera
+ *      Date            -   14/08/2020
+ *      Description     -   Handle the result of CRUD function. 
+ *      
+ *      Version Control
+ *          * [Kusal Perera]
+ *              Implement the common class to handle the result of CRUD function.
+ *      
+ */
+
+namespace TimeTable_App.Global
+{
+    public class ActionResult
+    {
+        public bool State { get; set; }
+        public dynamic Data { get; set; }
+    }
+}
