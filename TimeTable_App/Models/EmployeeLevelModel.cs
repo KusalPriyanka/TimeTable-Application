62a07dae51f25de41bc5fa7bbfaad64fc4f256ea
KusalPriyanka <kusalpriyanka782@gmail.com>
2020-08-16 12:05:05 +0530

    add employee level class

---
+++ TimeTable_App/Models/EmployeeLevelModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace TimeTable_App.Models
+{
+    public class EmployeeLevelModel
+    {
+        [Key]
+        public int EmployeeLevelId { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string EmployeeLevelName { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string EmployeeLevelDesc { get; set; }
+    }
+}
