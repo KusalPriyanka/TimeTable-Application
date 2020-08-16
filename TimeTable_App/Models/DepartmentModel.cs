91fc6b2e2343e967c63deeedaa351c1fa53e9028
KusalPriyanka <kusalpriyanka782@gmail.com>
2020-08-16 12:04:39 +0530

    Add department model class

---
+++ TimeTable_App/Models/DepartmentModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace TimeTable_App.Models
+{
+    public class DepartmentModel
+    {
+        [Key]
+        public int DepartmentId { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string DepartmentName { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string DepartmentDesc { get; set; }
+        [Required]
+        public int FacultyId { get; set; }
+    }
+}
