fb3b93ea6e4d2a85c9270ad564c927ecb7f24493
KusalPriyanka <kusalpriyanka782@gmail.com>
2020-08-16 12:04:03 +0530

    Add Building model

---
+++ TimeTable_App/Models/CenterModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace TimeTable_App.Models
+{
+    public class CenterModel
+    {
+        [Key]
+        public int CenterId { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string CenterName { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string CenterDesc { get; set; }
+    }
+}
