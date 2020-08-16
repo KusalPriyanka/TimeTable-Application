49ff5f99d14c144ed6cf49e1cd84c52d6a982311
KusalPriyanka <kusalpriyanka782@gmail.com>
2020-08-16 12:03:37 +0530

    Add Building model

---
+++ TimeTable_App/Models/BuildingModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace TimeTable_App.Models
+{
+    public class BuildingModel
+    {
+        [Key]
+        public int BuildingId { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string BuildingName { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string BuildingDesc { get; set; }
+    }
+}
