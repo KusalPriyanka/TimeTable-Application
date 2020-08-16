c345c825a593fb1027938d38af667a69ba321faa
KusalPriyanka <kusalpriyanka782@gmail.com>
2020-08-16 12:05:26 +0530

    add faculty class

---
+++ TimeTable_App/Models/FacultyModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace TimeTable_App.Models
+{
+    public class FacultyModel
+    {
+        [Key]
+        public int FacultyId { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string FacultyName { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string FacultyDesc { get; set; }
+    }
+}
