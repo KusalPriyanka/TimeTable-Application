76a16171231d55e879ca438444d014fb2e7b11b4
KusalPriyanka <kusalpriyanka782@gmail.com>
2020-08-16 23:35:43 +0530

    implement get date methods in LecturersFormModel

--- TimeTable_App/Models/LecturersFormModel.cs
+++ TimeTable_App/Models/LecturersFormModel.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
 using System.Text;
+using TimeTable_App.Global;
 
 namespace TimeTable_App.Models
 {
@@ -32,5 +34,46 @@ namespace TimeTable_App.Models
         [Required]
         [MaxLength(8)]
         public string Rank { get; set; }
+
+        public LecturersFormModel GetId(LecturersFormModel obj)
+        {
+            TimeTableDbContext _dbContext = new TimeTableDbContext();
+            int exitCount = _dbContext.Lecturers.Count() + 1;
+            obj.EmployeeId = exitCount.ToString().PadLeft(6, '0');
+            obj.Rank = obj.EmployeeLevel + "." + obj.EmployeeId;
+            return obj;
+        }
+
+        public dynamic GetFormData(TimeTableDbContext _dbContext, string type) 
+        {
+            if (type == "Faculty")
+            {
+                return _dbContext.Faculties.AsNoTracking().ToList();
+            }
+            else if (type == "Department")
+            {
+                return _dbContext.Departments.AsNoTracking().ToList();
+            }
+            else if (type == "Center")
+            {
+                return _dbContext.Centers.AsNoTracking().ToList();
+            }
+            else if (type == "Building")
+            {
+                return _dbContext.Buildings.AsNoTracking().ToList();
+            }
+            else if (type == "Level")
+            {
+                return _dbContext.EmployeeLevels.AsNoTracking().ToList();
+            }
+            else if (type == "Lecturers") 
+            {
+                return _dbContext.Lecturers.AsNoTracking().ToList();
+            }
+
+            return null;
+        }
+
     }
+
 }
