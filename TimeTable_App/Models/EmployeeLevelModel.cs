662a350f2fb83d236524aed394047f22aeea230f
KusalPriyanka <kusalpriyanka782@gmail.com>
2020-08-18 05:57:39 +0530

    create EmployeeLevelModel

--- TimeTable_App/Models/EmployeeLevelModel.cs
+++ TimeTable_App/Models/EmployeeLevelModel.cs
@@ -3,6 +3,18 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Text;
 
+/*
+ *      Class Name      -   EmployeeLevelModel
+ *      Author          -   Kusal Perera
+ *      Date            -   18/08/2020
+ *      Description     -   Handle the details about the employee levels. 
+ *      
+ *      Version Control
+ *          * [Kusal Perera]
+ *              Implement the class to handle employee levels details.
+ *      
+ */
+
 namespace TimeTable_App.Models
 {
     public class EmployeeLevelModel
