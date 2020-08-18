260107e80a59a44ceae9bab809875269be118864
AshenSenevirathne <ashensachin@gmail.com>
2020-08-18 18:03:26 +0530

    Model created for Academic Year and Semester

---
+++ TimeTable_App/Models/ASYModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using TimeTable_App.Global;
+
+
+namespace TimeTable_App.Models
+{
+    public class ASYModel
+    {
+        [Key]
+        public int ASYID { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Year { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Semester { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string YS { get; set; }
+
+        public dynamic GetFormData(TimeTableDbContext _dbContext, string type)
+        {
+            if (type == "ASY")
+            {
+                return _dbContext.ASY.AsNoTracking().ToList();
+            }
+
+            return null;
+        }
+    }
+
+
+}
