1ea92c7bd792eb94622b89981a9e9005379f4595
AshenSenevirathne <ashensachin@gmail.com>
2020-08-18 11:10:29 +0530

    Programme Model Created

---
+++ TimeTable_App/Models/ProgrammeModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using TimeTable_App.Global;
+
+namespace TimeTable_App.Models
+{
+    public class ProgrammeModel
+    {
+        [Key]
+        public int ProgrammeId { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Programme { get; set; }
+
+        public dynamic GetFormData(TimeTableDbContext _dbContext, string type)
+        {
+            if (type == "Programme")
+            {
+                return _dbContext.Programme.AsNoTracking().ToList();
+            }
+
+            return null;
+        }
+    }
+
+
+}
